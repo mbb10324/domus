ada6ed00089be61df3d48b39c870bc00ee0e6334
Your Name <you@example.com>
2024-04-18 20:44:27 -0400

    deployment ready

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,7 @@ function App() {
 	const [showLegalsModal, setShowLegalsModal] = useState(false);
 	const [selectedLink, setSelectedLink] = useState<Website | null>(null);
 	const [orderMode, setOrderMode] = useState(false);
-	const storedWebsites: Website[] = localStorage.getItem("websites")
-		? JSON.parse(localStorage.getItem("websites")!)
-		: [];
+	const storedWebsites: Website[] = localStorage.getItem("websites") ? JSON.parse(localStorage.getItem("websites")!) : [];
 	const [websites, setWebsites] = useState(storedWebsites);
 	const [longPressTimer, setLongPressTimer] = useState<number | null>(null);
 	const [isLongPress, setIsLongPress] = useState(false);
@@ -90,17 +88,8 @@ function App() {
 							onTouchStart={(e) => handleLongPressStart(website, e)}
 							onTouchEnd={handleLongPressEnd}
 						>
-							{showEdit === index && (
-								<Edit setShowEditModal={setShowEditModal} thisLink={website} setSelectedLink={setSelectedLink} />
-							)}
-							<a
-								href={website.url}
-								target="_blank"
-								rel="noreferrer"
-								key={index}
-								className="link-wrapper"
-								onContextMenu={(e) => e.preventDefault()}
-							>
+							{showEdit === index && <Edit setShowEditModal={setShowEditModal} thisLink={website} setSelectedLink={setSelectedLink} />}
+							<a href={website.url} key={index} className="link-wrapper" onContextMenu={(e) => e.preventDefault()}>
 								<div className="img-wrapper">
 									<img
 										src={`https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${website.url}/&size=32`}
--- src/mocksites.ts
+++
@@ -1,1 +0,0 @@
-export const defaultWebsites = [{ name: "Google", url: "https://www.google.com", image: "https://www.google.com/favicon.ico" }];
